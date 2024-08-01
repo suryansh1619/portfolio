da2e66f6ad25dce1a9c5fce6bbba9dd5e7aaac1a
suryansh1619 <suryanshsahu2812@gmail.com>
2024-08-01 18:30:31 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -22,12 +22,12 @@ import Setting from './setting/Setting';
 function App() {
   const [isLoading, setLoading] = useToggle(true);
   const [data, setData] = useState({});
-  const [color,setColor] =useState(null)
+  const [color, setColor] = useState(null)
 
   const saveData = (data) => {
     setData(data);
   }
-  const baseURL =process.env.REACT_APP_BACKEND_PORT || '';
+  const baseURL = process.env.REACT_APP_BACKEND_PORT || '';
   const getData = async () => {
     try {
       const response = await axios.get(`${req.protocol}://${req.get("host")}/api/portfolio/data`)
@@ -42,19 +42,19 @@ function App() {
   useEffect(() => {
     getData()
   }, []);
-  const loaction=useLocation();
+  const loaction = useLocation();
   const isAdminRoute = loaction.pathname.startsWith('/admin');
   useEffect(() => {
     if (data.theme && Array.isArray(data.theme)) {
-        const selectedTheme = data.theme.find(theme => theme.selected);
-        if (selectedTheme) {
-            setColor(selectedTheme.color);
-        }
+      const selectedTheme = data.theme.find(theme => theme.selected);
+      if (selectedTheme) {
+        setColor(selectedTheme.color);
+      }
     }
   }, [data.theme]);
-    useEffect(()=>{
-      document.documentElement.style.setProperty('--first-color',color);
-    },[color]);
+  useEffect(() => {
+    document.documentElement.style.setProperty('--first-color', color);
+  }, [color]);
   return (
     <div>
       <ThemeProvider>
@@ -86,38 +86,38 @@ function App() {
                 element={<Setting
                   themes={data.theme}
                   color={color}
-                  setColor={setColor}/>} />
-              <Route 
-                path='/admin/home' 
-                element={<AdminHome 
+                  setColor={setColor} />} />
+              <Route
+                path='/admin/home'
+                element={<AdminHome
                   homeData={data.homeData[0]}
-                  homeSocial={data.homeSocial}/>} />
-              <Route 
-                path='/admin/about' 
-                element={<AdminAbout  
+                  homeSocial={data.homeSocial} />} />
+              <Route
+                path='/admin/about'
+                element={<AdminAbout
                   about={data.about[0]}
-                  aboutInfo={data.aboutInfo[0]}/>} />
-              <Route 
-                path='/admin/skills' 
-                element={<AdminSkill  
-                  skills={data.skills}/>} />
-              <Route 
-                path='/admin/qualification' 
-                element={<AdminQualification  
+                  aboutInfo={data.aboutInfo[0]} />} />
+              <Route
+                path='/admin/skills'
+                element={<AdminSkill
+                  skills={data.skills} />} />
+              <Route
+                path='/admin/qualification'
+                element={<AdminQualification
                   qualificationEducation={data.qualificationEducation}
-                  qualificationExperience={data.qualificationExperience}/>} />
-              <Route 
-                path='/admin/projects' 
-                element={<AdminProjects 
-                  projectsProject={data.projectsProject}/>} />
-              <Route 
-                path='/admin/footer' 
+                  qualificationExperience={data.qualificationExperience} />} />
+              <Route
+                path='/admin/projects'
+                element={<AdminProjects
+                  projectsProject={data.projectsProject} />} />
+              <Route
+                path='/admin/footer'
                 element={<AdminFooter
-                  footer={data.footer}/>} />
-              <Route 
-                path='/admin/theme' 
+                  footer={data.footer} />} />
+              <Route
+                path='/admin/theme'
                 element={<AdminTheme
-                  theme={data.theme}/>} />
+                  theme={data.theme} />} />
               <Route
                 path="*"
                 element={<h1>Error Not Found</h1>} />
