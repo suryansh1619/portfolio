bb582224800df1846dd1eebf46530cdbd3a84be7
suryansh1619 <118927234+suryansh1619@users.noreply.github.com>
2024-08-06 22:47:51 +0530

    Update jwt.js

--- jwt.js
+++ jwt.js
@@ -1,22 +1,19 @@
 const jwt = require('jsonwebtoken');
 
-const jwtauth = async (req, res, next) => {
+const jwtauth =async (req, res, next) => {
     const token = req.cookies.token; 
-    if (!token) {
-        console.log('No token found in cookies');
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
+    console.log(token);
+    if (!token) return res.status(401).json({ message: 'Unauthorized' });
 
     try {
         const user = jwt.verify(token, process.env.JWT_SECRET);
         req.user = user; 
         next(); 
     } catch (err) {
-        console.error('Token verification failed:', err);
+        console.error(err);
         res.status(401).json({ error: 'Invalid Token' });
     }
 }
-
 const generatetoken = (data) => {
     return jwt.sign(data, process.env.JWT_SECRET, { expiresIn: '1h' });
 }
