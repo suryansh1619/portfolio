bfd5b3a498006364ff850280640429f8eedbb8d2
suryansh1619 <suryanshsahu2812@gmail.com>
2024-08-07 20:33:30 +0530

    jwt.js

--- jwt.js
+++ jwt.js
@@ -1,8 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const jwtauth =async (req, res, next) => {
-    // const token = req.cookies.token; 
-    const token=req.headers.authorization.split(" ")[1];
+    const token = req.cookies.token; 
     if (!token) return res.status(401).json({ message: 'Unauthorized' });
 
     try {
